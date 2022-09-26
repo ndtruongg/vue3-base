3a5e495030e670c627e80117415389f851d356b8
truongnd2-fabbi <truongnd2@fabbi.io>
2022-09-26 09:32:38 +0700

    add tabs component

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../../assets/images/logo.png" />
   </div>
+
+  <TabsWrapper>
+    <Tab title="Tab 1">Hello from tab 1</Tab>
+    <Tab title="Tab 2">Hello from tab 2</Tab>
+    <Tab title="Tab 3">Hello from tab 3</Tab>
+    <Tab title="Tab 4">Hello from tab 4</Tab>
+  </TabsWrapper>
 </template>
 
-<script setup></script>
+<script setup>
+import TabsWrapper from '../molecules/TabsWrapper.vue';
+import Tab from '../molecules/Tab.vue';
+</script>
